c23688a476fa57671163264e4946b3b3dee831e1
mingg123 <spgb123spgb@gmail.com>
2022-03-11 00:46:26 +0900

    feat : add touch event

--- app.js
+++ app.js
@@ -28,6 +28,114 @@ function startPainting() {
   painting = true;
 }
 
+var ongoingTouches = [];
+
+function copyTouch(touch) {
+  return {
+    identifier: touch.identifier,
+    pageX: touch.pageX,
+    pageY: touch.pageY,
+  };
+}
+
+function ongoingTouchIndexById(idToFind) {
+  for (var i = 0; i < ongoingTouches.length; i++) {
+    var id = ongoingTouches[i].identifier;
+    if (id == idToFind) {
+      return i;
+    }
+  }
+  return -1; // not found
+}
+
+function onTouchMove(evt) {
+  evt.preventDefault();
+  var touches = evt.changedTouches;
+
+  for (var i = 0; i < touches.length; i++) {
+    // var color = colorForTouch(touches[i]);
+    var idx = ongoingTouchIndexById(touches[i].identifier);
+
+    if (idx >= 0) {
+      console.log('continuing touch ' + idx);
+      ctx.beginPath();
+      console.log(
+        'ctx.moveTo(' +
+          ongoingTouches[idx].pageX +
+          ', ' +
+          ongoingTouches[idx].pageY +
+          ');'
+      );
+      ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
+      console.log(
+        'ctx.lineTo(' + touches[i].pageX + ', ' + touches[i].pageY + ');'
+      );
+      ctx.lineTo(touches[i].pageX, touches[i].pageY);
+      ctx.lineWidth = 4;
+      // ctx.strokeStyle = color;
+      ctx.stroke();
+
+      ongoingTouches.splice(idx, 1, copyTouch(touches[i])); // swap in the new touch record
+      console.log('.');
+    } else {
+      console.log("can't figure out which touch to continue");
+    }
+  }
+}
+
+function onTouchStart(evt) {
+  evt.preventDefault();
+  console.log('touchstart.');
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  } else {
+    var touches = evt.changedTouches;
+
+    for (var i = 0; i < touches.length; i++) {
+      console.log('touchstart:' + i + '...');
+      ongoingTouches.push(copyTouch(touches[i]));
+      // var color = colorForTouch(touches[i]);
+      ctx.beginPath();
+      ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false); // a circle at the start
+      // ctx.fillStyle = color;
+      ctx.fill();
+      console.log('touchstart:' + i + '.');
+    }
+  }
+}
+
+function onTouchEnd(evt) {
+  evt.preventDefault();
+  console.log('touchend');
+
+  var touches = evt.changedTouches;
+
+  for (var i = 0; i < touches.length; i++) {
+    var idx = ongoingTouchIndexById(touches[i].identifier);
+
+    if (idx >= 0) {
+      ctx.beginPath();
+      ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
+      ctx.lineTo(touches[i].pageX, touches[i].pageY);
+      ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8); // and a square at the end
+      ongoingTouches.splice(idx, 1); // remove it; we're done
+    } else {
+      console.log("can't figure out which touch to end");
+    }
+  }
+}
+
+function onTouchCancel(evt) {
+  evt.preventDefault();
+  console.log('touchcancel.');
+  var touches = evt.changedTouches;
+
+  for (var i = 0; i < touches.length; i++) {
+    var idx = ongoingTouchIndexById(touches[i].identifier);
+    ongoingTouches.splice(idx, 1); // remove it; we're done
+  }
+}
+
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
@@ -40,20 +148,6 @@ function onMouseMove(event) {
   }
 }
 
-function onTouchMove(event) {
-  if (event.target == canvas) {
-    event.preventDefault();
-  }
-  const x = event.offsetX;
-  const y = event.offsetY;
-  if (!painting) {
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-  } else {
-    ctx.lineTo(x, y);
-    ctx.stroke();
-  }
-}
 //mouseDown이 마우스를 클릭하는 순간
 function onMouseDown(event) {
   painting = true;
@@ -106,16 +200,15 @@ if (canvas) {
   canvas.addEventListener('click', handleCanvasClick);
   canvas.addEventListener('contextmenu', handleContextMenu);
 
+  canvas.addEventListener('touchstart', onTouchStart, false);
+  canvas.addEventListener('touchend', onTouchEnd, false);
+  canvas.addEventListener('touchcancel', onTouchCancel, false);
   canvas.addEventListener('touchmove', onTouchMove, false);
-  canvas.addEventListener('touchstart', onMouseDown, false);
-  canvas.addEventListener('touchend', onMouseUp, false);
-  canvas.addEventListener('touchcancel', onMouseLeave, false);
 }
 if (colors) {
   Array.from(colors).forEach(color =>
     color.addEventListener('click', handleColorClick)
   );
-  console.log('click!');
 }
 
 if (range) {
